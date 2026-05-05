b9c15bca32d83c886b91ecd075e454e9ee4c78b7
sararadulovic03 <radulovicsara232@gmail.com>
2026-05-05 10:17:30 +0200

    Dodate metode hashCode, equals i toString u klasama Knjiga i Autor

--- src/biblioteka/Autor.java
+++ src/biblioteka/Autor.java
@@ -1,23 +1,48 @@
 package biblioteka;
 
+import java.util.Objects;
+
 public class Autor {
-	
+
 	private String ime;
 	private String prezime;
-	
-	
+
 	public String getIme() {
 		return ime;
 	}
+
 	public void setIme(String ime) {
 		this.ime = ime;
 	}
+
 	public String getPrezime() {
 		return prezime;
 	}
+
 	public void setPrezime(String prezime) {
 		this.prezime = prezime;
 	}
-	
-	
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(ime, prezime);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Autor other = (Autor) obj;
+		return Objects.equals(ime, other.ime) && Objects.equals(prezime, other.prezime);
+	}
+
+	@Override
+	public String toString() {
+		return "Autor [ime=" + ime + ", prezime=" + prezime + "]";
+	}
+
 }
--- src/biblioteka/Knjiga.java
+++ src/biblioteka/Knjiga.java
@@ -1,6 +1,7 @@
 package biblioteka;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Knjiga {
 
@@ -48,6 +49,30 @@ public class Knjiga {
 
 	public void setIzdanje(int izdanje) {
 		this.izdanje = izdanje;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(autori, isbn, izdanje, izdavac, naslov);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Knjiga other = (Knjiga) obj;
+		return Objects.equals(autori, other.autori) && isbn == other.isbn && izdanje == other.izdanje
+				&& Objects.equals(izdavac, other.izdavac) && Objects.equals(naslov, other.naslov);
+	}
+
+	@Override
+	public String toString() {
+		return "Knjiga [naslov=" + naslov + ", isbn=" + isbn + ", autori=" + autori + ", izdavac=" + izdavac
+				+ ", izdanje=" + izdanje + "]";
 	}
 
 }
