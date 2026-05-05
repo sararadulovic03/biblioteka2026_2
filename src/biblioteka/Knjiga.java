4a70f8e788cce5262263a80fd60e28746ade0055
sararadulovic03 <radulovicsara232@gmail.com>
2026-05-05 10:11:36 +0200

    Dodata klasa Autor i refaktorisana klasa Knjiga

--- src/biblioteka/Knjiga.java
+++ src/biblioteka/Knjiga.java
@@ -1,44 +1,53 @@
 package biblioteka;
 
+import java.util.List;
+
 public class Knjiga {
-	
+
 	private String naslov;
 	private long isbn;
-	private String autori;
+	private List<String> autori;
 	private String izdavac;
 	private int izdanje;
-	
+
 	public String getNaslov() {
 		return naslov;
 	}
+
 	public void setNaslov(String naslov) {
 		this.naslov = naslov;
 	}
+
 	public long getIsbn() {
 		return isbn;
 	}
+
 	public void setIsbn(long isbn) {
 		this.isbn = isbn;
 	}
-	public String getAutori() {
+
+	public List<String> getAutori() {
 		return autori;
 	}
-	public void setAutori(String autori) {
+
+	public void setAutori(List<String> autori) {
 		this.autori = autori;
 	}
+
 	public String getIzdavac() {
 		return izdavac;
 	}
+
 	public void setIzdavac(String izdavac) {
 		this.izdavac = izdavac;
 	}
+
 	public int getIzdanje() {
 		return izdanje;
 	}
+
 	public void setIzdanje(int izdanje) {
 		this.izdanje = izdanje;
 	}
-	
-	
-	
+
 }
